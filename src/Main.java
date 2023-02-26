aa4609b175e4b1861a5e76c9ff3fa52af6b5139d
AleksandrVashchilo <suszycki.alexander@gmail.com>
2023-02-26 18:55:26 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,86 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+   mergeSort(arr) - function that sorts arr[leftIndex..rightIndex] using merge()
+   merge(arr, leftArr, rightArr) - Merges two sub arrays of arr[].
+    */
+
+        int[] arr = {3, 8, 1, 9, 4, 2,7,6,5};
+        System.out.println("Given Array");
+        System.out.println(Arrays.toString(arr));
+
+        mergeSort(arr);
+        System.out.println("Sorted array");
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void mergeSort(int[] arr) {
+        int lengthArr = arr.length; // длина массива
+
+        if (lengthArr == 1) { // условие выхода
+            return;
+        }
+
+        int mid = lengthArr / 2;
+        int[] leftArr = new int[mid]; // левый подмассив*
+        int[] rightArr = new int[lengthArr - mid]; // правый подмассив*
+
+        // копируем элементы из массива в подмассивы
+        for (int i = 0; i < mid; ++i) {
+            leftArr[i] = arr[i];
+        }
+        for (int j = 0; j < lengthArr - mid; ++j) {
+            rightArr[j] = arr[j + mid];
+        }
+
+
+        mergeSort(leftArr);
+        mergeSort(rightArr);
+
+        // когда достигли базового условия, начинаем слияние
+        merge(arr, leftArr, rightArr);
+    }
+
+    private static void merge(int[] arr, int[] leftArr, int[] rightArr) {
+        int leftArrLength = leftArr.length;
+        int rightArrLength = rightArr.length;
+
+        // контролируем индексы подмассивов
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        // контролируем индекс в основном массиве
+        int arrIndex = 0;
+
+
+        while (leftIndex < leftArrLength && rightIndex < rightArrLength) {
+            if (leftArr[leftIndex] > rightArr[rightIndex]) { // сравниваем элемент в левом и правом подмассиве
+                arr[arrIndex] = leftArr[leftIndex]; // если условие верно, то элемент в левом меньше, сохраняем его в 0 ячейку основного массива
+                leftIndex++;
+                arrIndex++;
+            } else {
+                arr[arrIndex] = rightArr[rightIndex];
+                rightIndex++;
+                arrIndex++;
+            }
+        }
+
+        // копируем элементы если остались из левого
+        while (leftIndex < leftArrLength) {
+            arr[arrIndex] = leftArr[leftIndex];
+            leftIndex++;
+            arrIndex++;
+        }
+
+        // копируем элементы из правого
+        while (rightIndex < rightArrLength) {
+            arr[arrIndex] = rightArr[rightIndex];
+            rightIndex++;
+            arrIndex++;
+        }
     }
 }
